b1f604d8ed5edf06fdf0a9ec9a1d43dff3f2fe69
Koh-Du-Beom <140967208+Koh-Du-Beom@users.noreply.github.com>
2026-08-04 09:06:39 +0900

    fix : 이력서 노출하지 않게 수정

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import { notFound } from "next/navigation";
 
 export default function ResumePage() {
-  redirect("/resume/index.html");
+  notFound();
 }
